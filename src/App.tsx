adebb54eb595b5e474a30d71839319d5a6e88a3b
Igor Voronkov <igorvoronkov86@gmail.com>
2020-05-12 01:47:05 +0300

    Added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { NavLink, Redirect, Route, Switch } from 'react-router-dom';
 import AddProductForm from './features/products/AddProductForm';
 import ProductsList from './features/products/ProductsList';
 
 export default function App() {
   return (
     <div>
-      <AddProductForm />
-      <hr />
-      <ProductsList />
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Products list</NavLink>
+          </li>
+          <li>
+            <NavLink to="/add-new-product">Add new product</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/products" />
+        </Route>
+        <Route path="/products">
+          <ProductsList />
+        </Route>
+        <Route path="/add-new-product">
+          <AddProductForm />
+        </Route>
+      </Switch>
     </div>
   );
 }
